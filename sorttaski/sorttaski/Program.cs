3ac7b49b7fb1bf2b0d287cd8b1fb181ca3ea9cf6
unknown <azizovagultakin@gmail.com>
2023-03-03 03:17:50 +0300

    first commit

---
+++ sorttaski/sorttaski/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Immutable;
+//1.Kiçikdən böyüyə siralanmış array-i böyükdən kiçiyə sıralayan alqoritm.
+//int[] arr = { 2, 5, 7, 9, 12, 16, 19, 21, 28, 31, 35 };
+//Array.Reverse(arr);
+//for (int i = 0; i < arr.Length; i++)
+//    Console.WriteLine(arr[i]);
+
+
+
+//2. Ədəd və rəqəmlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin 
+//int[] arr= {3, 5, 16, 24,8,34 };
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < 10)
+//    {
+
+
+//        Console.WriteLine(arr[i]);
+//    }
+}
